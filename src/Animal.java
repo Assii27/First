b1b5ec62d6eec3be57f699fcf55924e8e84a41d5
Asif_Maner <manerasif786@gmail.com>
2023-05-26 08:14:32 +0530

    First Inteliji Project

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Animal {
+
+
+class CheckVoice {
+    public void Voice(){
+        System.out.println("Selected Animal Voice");
+    }
+}
+
+class Cat extends CheckVoice{
+
+    @Override
+    public void Voice() {
+        System.out.println("Cat Voice");
+    }
+}
+
+class Dog extends CheckVoice{
+
+    @Override
+    public void Voice() {
+        System.out.println("Dog Voice");
+    }
+}
+public class Animal {
+    public static void main(String[] args) {
+        System.out.println(" This Is Main Class-: ");
+
+      CheckVoice animal=new Cat();
+        animal.Voice();
+
+        animal=new Dog();
+        animal.Voice();
+
+    }
 }
